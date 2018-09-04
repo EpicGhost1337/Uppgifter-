59e63fe281116586676c45851de5b8dd8a379529
EpicGhost1337 <andy.ung.te16d@gmail.com>
2018-09-04 20:12:58 +0200

    Uppgift 2

---
+++ src/Uppgift_2.java
@@ -0,0 +1,22 @@
+
+public class Uppgift_2 {
+
+		
+	public static void main(String[] args) {
+		
+		System.out.println(reverse("Baok"));	
+	}
+	
+	public static String reverse(String input) {
+		
+		if (input.length() == 1) {
+			
+			return input;
+		}
+		
+		else {
+			return reverse(input.substring(1))+ input.charAt(0);
+		}
+    }		
+}
+
