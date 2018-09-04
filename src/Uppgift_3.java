577989c7f90aed5fda7d6b0768435bc5e815519e
EpicGhost1337 <andy.ung.te16d@gmail.com>
2018-09-04 20:13:11 +0200

    uppgift 3

---
+++ src/Uppgift_3.java
@@ -0,0 +1,16 @@
+
+public class Uppgift_3 {
+	
+	public static void main(String[] args) {
+		
+		generic();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[] generic(T... generic) {
+		
+		return generic;
+
+}
+
+}
